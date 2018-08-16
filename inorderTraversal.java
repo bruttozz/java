b1dc0cb732ca052702d58a8711eb39555809573b
HONGTAO ZHAO <32073310+bruttozz@users.noreply.github.com>
2018-08-16 16:10:17 +0800

    Add files via upload

---
+++ inorderTraversal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root == null){
+            return res;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        pushLeft(root, stack);
+        while(!stack.isEmpty()){
+            TreeNode first = stack.removeFirst();
+            res.add(first.val);
+            pushLeft(first.right, stack);
+        }
+        return res;
+    }
+
+    private void pushLeft(TreeNode root, Deque<TreeNode> stack){
+        while(root != null){
+            stack.addFirst(root);
+            root = root.left;
+        }
+
+    }
+}
