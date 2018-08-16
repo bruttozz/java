e65079315a13ab895969ce293e77662e79c40132
HONGTAO ZHAO <32073310+bruttozz@users.noreply.github.com>
2018-08-16 14:15:23 +0800

    Add files via upload

---
+++ deque.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class deque {
+  public static void main(String[] args){
+    Deque a = new ArrayDeque<>();
+    a.add("element 0");
+    a.addFirst("element 1");
+    a.addFirst("element 2");
+    a.addLast("element 3");
+    a.add("element 4");
+    /*Iterator iterator = a.iterator();
+    while(iterator.hasNext()){
+      //int b = (int)a.iterator();
+      String element = (String) iterator.next();
+      System.out.println(element);
+    }*/
+    for(Object object : a){
+      System.out.println(object);
+    }
+  }
+}
